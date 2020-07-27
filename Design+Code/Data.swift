7e68a2ffdf482be57658d7177775e170b3de7003
Parshva Shah <shahparshva72@gmail.com>
2020-07-27 14:50:08 +0530

    Completed the Course

---
+++ Design+Code/Data.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIView.swift
+//  Design+Code
+//
+//  Created by Parshva Shah on 7/3/20.
+//  Copyright © 2020 Parshva Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var title: String
+    var body: String
+}
+
+class Api {
+    func getPosts(completion: @escaping ([Post]) -> ()) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, _ , _) in
+            guard let data = data else {return}
+            
+            let posts = try! JSONDecoder().decode([Post].self, from: data)
+            
+            DispatchQueue.main.async {
+                completion(posts)
+            }
+        }
+        .resume()
+    }
+}
